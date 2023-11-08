1e59d87631a58cbf19d590302981a68f9837d55e
TonyDeleo98 <Tonyd9819@gmail.com>
2023-11-07 22:21:33 -0500

    updated file

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const routes = require('./routes');
+const mysql = require("mysql2");
 // import sequelize connection
 const sequelize = require('./config/connection');
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3001,
+  user: "root",
+  password: "Phillies_21",
+  database: "ecommerce_db;",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the database!");
+  
+  start();
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
